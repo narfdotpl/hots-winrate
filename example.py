ee32ff439c1de270bce190c035b27ca7e95895b9
Maciej Konieczny <hello@narf.pl>
2019-08-27 09:03:34 +0200

    focused on Sylvanas, at least 10 games

--- example.py
+++ example.py
@@ -1,80 +1,23 @@
 from api import *
 
 
-games = load_games(owner="narf")  # use your name
+games = load_games(owner="narf").filter(as_("Sylvanas"))
 
-print "Fenix games:", games.filter(lambda game: game.owner.hero == "Fenix")
-print "Fenix games:", games.filter(owner.hero("Fenix"))
-print "Fenix games:", games.filter(as_("Fenix"))
+print '## Overall Sylvanas winrate\n'
+print games
+print
 print
 
-print "Fenix games by map:"
-print games.filter(as_("Fenix")).by_map()
+at_least = 10
+print_synergies(games, at_least)
+print_counters(games, at_least)
+
+print '## Maps\n'
+print games.by_map().at_least(at_least)
 print
 
-print "Fenix games on Volskaya:", games.filter(as_("Fenix") & map_(maps.volskaya))
-print "Fenix games on Volskaya:", games.filter(as_("Fenix") & on(maps.volskaya))
-print
-
-fenix_games = games.filter(as_("Fenix"))
-print "Fenix games by teammate:"
-print fenix_games.by_teammate_hero().at_least(3)
-print
-
-print "Fenix games by enemy:"
-print fenix_games.by_enemy_hero().at_least(3)
-print
-
-print "All games by enemy:"
-print games.by_enemy_hero().at_least(5)
-print
-
-print "Fenix + Guldan games:", games.filter(as_("Fenix") & with_("Gul'dan"))
-print "Greymane + Guldan games:", games.filter(as_("Greymane") & with_("Gul'dan"))
-print "Other + Guldan matches:", games.filter(~(as_("Fenix") | as_("Greymane")) & with_("Gul'dan"))
-print
-
-print "Fenix vs Varian:", games.filter(as_("Fenix") & vs("Varian"))
-print "vs Varian:"
-print games.filter(vs("Varian")).by_owner_hero()
-print
-
-Filip = teammate.player("jhgrng")
-Ziom = teammate.player("plasticbag")
-print "z Filipem i Ziomem:", games.filter(Filip & Ziom)
-print "tylko z Filipem:", games.filter(Filip & ~Ziom)
-print "tylko z Ziomem:", games.filter(Ziom & ~Filip)
-print "bez chlopakow:", games.filter(~Ziom & ~Filip)
-print
-
-print "z Filipem jako Guldanem:", games.filter(Filip.as_("Gul'dan"))
-print "z Ziomem jako Guldanem:", games.filter(Ziom.as_("Gul'dan"))
-print
-
-print "All games by map:"
-print games.by_map()
-print
-
-print_synergies(games)
-print_counters(games)
-print_heroes_by_map(games)
-
-print "Pairs with Filip:"
-print games.by_pairs(Filip).at_least(5)
-print
-
-print "Pairs with Ziom:"
+print '## Pairs with Ziom\n'
 print games.by_pairs(teammate_name="plasticbag").at_least(5)
 print
 
-print "Pairs in general:"
-print games.by_pairs().at_least(5)
-print
-
-print "Thrall maps:"
-print games.filter(as_("Thrall")).by_map().at_least(3)
-
-
-import os
-os.system('clear')
 print_days_of_the_week(games)

55a4a9f4cbf0078b2a48811bf27c2468085a27bd
Maciej Konieczny <hello@narf.pl>
2019-10-30 11:00:46 +0100

    added more people

--- narf.py
+++ narf.py
@@ -51,6 +51,6 @@ print all_games.filter(since(season1) & as_("Sylvanas")).by_friends([
     "dekusss",
     "Thax",
     "Tuddels", "Vesetoth",
-    "Snaps",
+    "Snaps", "leroilyche", "RisingSun",
     "Mike", "TempoKev", "kindaleek", "Chuppy", "NoctisRex",
-])
+]).at_least(5)
